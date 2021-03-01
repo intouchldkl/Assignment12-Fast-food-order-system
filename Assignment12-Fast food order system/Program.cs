27ad3a9bc137f2df161f7ce23b58029f86021d99
inn@ascot.ac.th <inn@ascot.ac.th>
2021-03-01 10:26:10 +0700

    Add project files.

---
+++ Assignment12-Fast food order system/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment12_Fast_food_order_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
